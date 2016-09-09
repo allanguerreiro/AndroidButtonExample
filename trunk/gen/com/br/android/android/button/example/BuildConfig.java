9510405b1e8485cd66b6bd483fcb05b7b642609c
Allan Guerreiro Carneiro <allanguerreiro@gmail.com>
2016-09-09 15:31:43 -0300

    Remove folders

--- trunk/gen/com/br/android/android/button/example/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.br.android.android.button.example;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- trunk/gen/com/br/android/android/button/example/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.br.android.android.button.example;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
